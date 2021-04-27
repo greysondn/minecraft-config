66dbbe2bbff743c299fdedb20bde79601c4b73ec
J. Dorian Greyson L <greysondn@gmail.com>
2021-04-27 05:26:45 -0400

    fixfiles - truly a glorious tool that

--- fixfiles.sh
+++ fixfiles.sh
@@ -1,5 +1,20 @@
+# sure are a lot of things to just check line endings on
 dos2unix -- ./*
 dos2unix -- ./config/*
 dos2unix -- ./config/*/*
 dos2unix -- ./config/*/*/*
 dos2unix -- ./config/*/*/*/*
+dos2unix -- ./patchouli_books/*
+dos2unix -- ./patchouli_books/*/*
+dos2unix -- ./patchouli_books/*/*/*
+dos2unix -- ./patchouli_books/*/*/*/*
+dos2unix -- ./patchouli_books/*/*/*/*/*
+dos2unix -- ./patchouli_books/*/*/*/*/*/*
+dos2unix -- ./world/serverconfig/*
+dos2unix -- ./world/serverconfig/*/*
+dos2unix -- ./openloader/*
+dos2unix -- ./openloader/*/*
+dos2unix -- ./openloader/*/*/*
+dos2unix -- ./openloader/*/*/*/*
+dos2unix -- ./openloader/*/*/*/*/*
+dos2unix -- ./openloader/*/*/*/*/*/*
