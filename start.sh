65d02a3befa067b02059d0d5d1957287409da923
J. 'Dorian Greyson' L <greysondn@gmail.com>
2021-04-29 04:16:25 +0000

    Whole ton of essentially meaningless changes

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xms8G -Xmx8G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:+DisableExplicitGC -jar ./forge-1.16.5-36.0.42.jar nogui
+java -Xms8G -Xmx8G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:+DisableExplicitGC -jar ./forge.jar nogui
