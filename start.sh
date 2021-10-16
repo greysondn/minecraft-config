fa12f2c027f091a676882fe692b0f22e94bd4b38
J. 'Dorian Greyson' L <greysondn@gmail.com>
2021-10-16 05:23:13 +0000

    mohist starts!

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xms8G -Xmx8G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:+DisableExplicitGC -jar ./forge.jar nogui
+java -Xms8G -Xmx8G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:+DisableExplicitGC -jar ./mohist-1.16.5-820-server.jar nogui
